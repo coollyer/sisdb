408d37707c8f5c00a12a2c710931bd550873240d
coollyer <zhengdong.ding@sci-inv.com>
2024-08-08 17:43:14 +0800

    replace win32

--- src/os/win32/sis_os.h
+++ src/os/win32/sis_os.h
@@ -2,19 +2,24 @@
 #ifndef _SIS_OS_H
 #define _SIS_OS_H
 
+#ifdef __MSVC__
 #pragma warning(disable: 4200 4267 4047 4996 4819 4244)
+#endif
 
 #define random rand
 #define inline __inline
 #define __func__  __FUNCTION__
 
+#ifndef __WIN32__
 #define __WIN32__
-
+#endif
 // ***  注意顺序不能变  *** //
 // #ifdef WINDOWS
 #include <winsock2.h>
 #include <windows.h>
+#ifdef __MSVC__
 #pragma comment (lib, "ws2_32.lib")
+#endif
 // ***  注意顺序不能变  *** //
 #include <errno.h>
 #include <assert.h>
